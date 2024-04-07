22deebb1aca6dab1e15d22ff9fbc6ea66b1fe41b
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-04-08 00:04:35 +0530

    first commit

---
+++ music_player_app/models.py
@@ -0,0 +1,15 @@
+# music_player_app/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+ 
+class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        related_query_name='user'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        related_query_name='user'
+    )
